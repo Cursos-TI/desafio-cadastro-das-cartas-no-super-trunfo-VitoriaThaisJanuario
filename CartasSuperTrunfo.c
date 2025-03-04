c2bb5f8abcf6c426bc86c4bb61bb1f3ff969059d
VitoriaThaisJanuario <202502562861@alunos.estacio.br>
2025-03-04 19:36:17 +0000

    EXIBINDO DADOS DAS CARTAS

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -54,6 +54,27 @@ int main() {
 
      printf("Número de Pontos Turistícos:\n");
      scanf("%d", &pontos2);
+       
+     
+     printf("\nCarta 1:\n");
+     printf("Estado: %c\n", estado1);
+     printf("Código: %s\n", codigo1);
+     printf("Nome da Cidade: %s\n", cidade1);
+     printf("População: %d\n", populacao1);
+     printf("Área: %.2f km²\n", area1);
+     printf("PIB: %.2f\n", pib1);
+     printf("Número de Pontos Turísticos: %d\n", pontos1);
+
+    
+     printf("\nCarta 2:\n");
+     printf("Estado: %c\n", estado2);
+     printf("Código: %s\n", codigo2);
+     printf("Nome da Cidade: %s\n", cidade2);
+     printf("População: %d\n", populacao2);
+     printf("Área: %.2f km²\n", area2);
+     printf("PIB: %.2f\n", pib2);
+     printf("Número de Pontos Turísticos: %d\n", pontos2);
+
 
    
     return 0;
