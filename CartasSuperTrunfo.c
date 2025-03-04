880c932f191256d61f2968b943e835e9091a4915
VitoriaThaisJanuario <202502562861@alunos.estacio.br>
2025-03-04 19:23:13 +0000

    Definindo a estrutura dentro da main

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
+    printf("Super Trunfo \n");
+   
+       // Definindo a estrutura dentro da main
+        char estado1, estado2;
+        char codigo1[5], codigo2[5];
+        char cidade1[50], cidade2[50];
+        int populacao1, populacao2;
+        float area1, area2;
+        float pib1, pib2;
+        int pontos1, pontos2;
+   
     return 0;
 }
